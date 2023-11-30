d3ec7cd308690e308f8a6ee767cfc434472958f1
Terts Diepraam <terts.diepraam@gmail.com>
2023-11-30 11:20:20 +0100

    add license headers and LICENSE file for derive

--- derive/src/initial.rs
+++ derive/src/initial.rs
@@ -1,3 +1,6 @@
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input, Data, DeriveInput, Fields, Token,
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 //! Argument parsing for the uutils coreutils project
 //!
 //! This crate provides the argument parsing for the
